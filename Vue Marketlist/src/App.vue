f4cd7051d2cf02b7402a287829c9a8f3a62c7ffb
Eduaina <eduaighalo29@gmail.com>
2023-07-15 22:51:45 +0100

    attempt1 at vue

---
+++ Vue Marketlist/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+const message =ref("My Marketlist")
+const show =ref(true)
+const list =ref(["bread","eggs","meat"])
+</script>
+
+<template>
+  <h1 >{{ message }}</h1>
+  <div>
+    <div>
+      <button @click="show = !show">Hide</button>
+      <button @click="list.push('air')">Expand</button>
+      <button @click="list.pop()">Reduce</button>
+   </div>
+   <ul v-if="show && list.length">
+   <li v-for="item of list">{{ item }}</li>
+   </ul>
+   <p v-else-if="list.length">You have hidden your market list.</p>
+   <p v-else>Nothing to see.</p>
+ </div>
+</template>
+
+<style>
+button, a {
+  display: block;
+  }
+</style>
